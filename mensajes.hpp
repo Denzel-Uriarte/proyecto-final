362be5ecbdfcfa1642213b5e7dfb8bd525b6209c
Denzel <denzeluriartem@gmail.com>
2024-11-22 09:36:05 -0800

    arreglado varios bugs, mas codigo

--- mensajes.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef mensajes_hpp
-#define mensajes_hpp
-
-#include <string>
-
-std::string conseguirMensaje(int mensaje);
-
-#endif
---
+++ textos.h
@@ -0,0 +1,9 @@
+#ifndef TEXTOS_H_INCLUDED
+#define TEXTOS_H_INCLUDED
+
+#include <string>
+
+std::string conseguirMensaje(int texto);
+std::string conseguirPasajeDeLibro(int pasaje);
+
+#endif // TEXTOS_H_INCLUDED
